6a64ef32a12ad5f7adf40e242e303edfcd9b3e1e
psuu0007@naver.com <psuu0007@naver.com>
2021-10-25 00:47:55 +0900

    흠 revert하니까 역시 commit만 왔네 이러면 음....

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		인덱스 1증가하겠지?
 //		한글이 안되네? 대박이다
 // 		ha bigbak is.
 //		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
